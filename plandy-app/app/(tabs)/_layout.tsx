34cbaca75fd837e8f8a56084129864894a4fb213
parkajeong <milk5253@gmail.com>
2026-05-19 13:27:45 +0900

    fix: 기본 Explore 화면 제거

--- plandy-app/app/(tabs)/_layout.tsx
+++ plandy-app/app/(tabs)/_layout.tsx
@@ -29,16 +29,6 @@ export default function TabLayout() {
       />
 
       <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
         name="subjects"
         options={{
           title: '과목',
@@ -47,6 +37,7 @@ export default function TabLayout() {
           ),
         }}
       />
+
       <Tabs.Screen
         name="todo"
         options={{
